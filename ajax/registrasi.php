428c57ab0a8ed08d4f65eb564d1fe79e969e3212
fawwazmuhammad <fawwazmuhammad@gmail.com>
2013-10-27 23:16:50 +0700

    url foto udah dtiambahin lagi nyoba konsep ajax

--- ajax/registrasi.php
+++ ajax/registrasi.php
@@ -12,7 +12,10 @@ mysql_select_db("waserda")or die("cannot select DB");
 // while ( $row = mysql_fetch_array($result)) {
 // 	print_r($row);
 // }
-$sql = "SELECT username FROM data_user";
+$username = $_POST["username"];
+// $sql = 'SELECT username FROM data_user WHERE `username` LIKE `dummy`';
+// ini gak tau kenapa masih error
+$sql = "SELECT username FROM data_user WHERE username LIKE `d%`";
 $result = mysql_query($sql);
 if (false === $result) {
     echo mysql_error();
