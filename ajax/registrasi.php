e6a2bd87e369a3fc3a67302a87504c78aea007af
fawwazmuhammad <fawwazmuhammad@gmail.com>
2013-10-27 21:24:02 +0700

    some database minor fix like auto increment

--- ajax/registrasi.php
+++ ajax/registrasi.php
@@ -1,9 +1,26 @@
 <?php
-$username=$_POST["username"];
-echo $username;
-$con=mysqli_connect("localhost","root","","waserda"); 
 
-$result = mysqli_query($con,"SELECT username,email FROM `data_user` WHERE username LIKE $username%");
-$hasil = mysql_fetch_array($result);
 
+mysql_connect("127.0.0.1", "root", "")or die("cannot connect");
+mysql_select_db("waserda")or die("cannot select DB");
+
+
+// $result = mysqli_query($con,"SELECT username,email FROM `data_user` WHERE username LIKE '$username'%");
+// $result = mysqli_query($con,"SELECT username,email FROM `data_user` ");
+
+
+// while ( $row = mysql_fetch_array($result)) {
+// 	print_r($row);
+// }
+$sql = "SELECT username FROM data_user";
+$result = mysql_query($sql);
+if (false === $result) {
+    echo mysql_error();
+}else{
+	$i=0;
+	while ($row=mysql_fetch_array($result)) {
+		echo $row["username"];
+		$i++;
+	}
+}
 ?>
